d9ce29bfe9e40a9fe9c1d7f3faf76f9176067568
David Begin <davidmichaelbe@gmail.com>
2015-05-17 20:51:45 -0700

    this is how I get ready

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 #[allow(unused_variables)]
 
 extern crate type_printer;
+mod problems;
 
 fn main() {
+    problems::im_here();
+}
+
+fn add_dem_pairs_up(vec_of_arrays: Vec<[i32; 2]>) -> Vec<i32> {
+    let mut result: Vec<i32> = vec_of_arrays.iter().map(|&arr| arr[0] * arr[1]).collect();
+    result
+}
+
+fn warmups() {
     // this is not valid,
     // we don't know the type
     // let first_vec = vec![];
@@ -17,12 +27,3 @@ fn main() {
     let all_yeah: Vec<[i32; 2]> = vec![[2, 3], [4, 5]];
     assert_eq!(add_dem_pairs_up(all_yeah), vec![6, 20]);
 }
-
-fn add_dem_pairs_up(vec_of_arrays: Vec<[i32; 2]>) -> Vec<i32> {
-    let mut result: Vec<i32> = vec_of_arrays.iter().map(|&arr| arr[0] * arr[1]).collect();
-    result
-}
-
-
-
-
---
+++ src/problems.rs
@@ -0,0 +1,3 @@
+pub fn im_here() {
+    println!("\nIM HERE\n");
+}
